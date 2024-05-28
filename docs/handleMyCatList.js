9f5b191fa9632d2866a46e7863d47b516960d02c
Ben Huang <benjaminwong1985@gmail.com>
2024-05-27 20:48:52 -0400

    feat: create a script to handle the display of my category list

---
+++ docs/handleMyCatList.js
@@ -0,0 +1,22 @@
+const previousOnLoad = window.onload;
+window.onload = () => {
+	previousOnLoad();
+	const sec1 = document.querySelector('#sec1section');
+	const catList = document.querySelector('.my-category-list');
+	if(sec1 && catList) {
+		catList.style.display = 'none';
+	} else if (catList) {
+		removeBr(catList);
+	}
+}
+function removeBr(node) {
+	if(node) {
+		if(node.localName == 'br') {
+			node.remove();
+		} else if (node.children) {
+			Array.from(node.children).forEach(child => {
+				removeBr(child);
+			});
+		}
+	}
+}
