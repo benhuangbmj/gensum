78fc9f2f952ba6fceea58567fe1a5687a9eceb26
Ben Huang <benjaminwong1985@gmail.com>
2024-05-28 21:56:06 -0400

    change: use previousOnLoad to preserve the window.onload function

--- docs/handleMyCatList.js
+++ docs/handleMyCatList.js
@@ -1,22 +1,24 @@
-const previousOnLoad = window.onload;
-window.onload = () => {
-	previousOnLoad();
-	const sec1 = document.querySelector('#sec1section');
-	const catList = document.querySelector('.my-category-list');
-	if(sec1 && catList) {
-		catList.style.display = 'none';
-	} else if (catList) {
-		removeBr(catList);
+{
+	const previousOnLoad = window.onload? window.onload : () => {};
+	window.onload = () => {
+		previousOnLoad();
+		const sec1 = document.querySelector('#sec1section');
+		const catList = document.querySelector('.my-category-list');
+		if(sec1 && catList) {
+			catList.style.display = 'none';
+		} else if (catList) {
+			removeBr(catList);
+		}
 	}
-}
-function removeBr(node) {
-	if(node) {
-		if(node.localName == 'br') {
-			node.remove();
-		} else if (node.children) {
-			Array.from(node.children).forEach(child => {
-				removeBr(child);
-			});
+	function removeBr(node) {
+		if(node) {
+			if(node.localName == 'br') {
+				node.remove();
+			} else if (node.children) {
+				Array.from(node.children).forEach(child => {
+					removeBr(child);
+				});
+			}
 		}
 	}
 }
